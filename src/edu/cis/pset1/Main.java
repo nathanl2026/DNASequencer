755109c52f70ab8178054256fd2b6b51fadff877
emlag <edl926@g.harvard.edu>
2019-02-17 17:28:04 +0800

    initial commit

---
+++ src/edu/cis/pset1/Main.java
@@ -0,0 +1,17 @@
+package edu.cis.pset1;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        DNAStrand myDna = new DNAStrand();
+//        Mrna myMRNA = new Mrna();
+//        myDna.readDNA("dnaSequence");
+//
+//        myMRNA.createCopy(myDna.getDnaSequence());
+//        ArrayList<String> protein = Ribosome.createProtein(myMRNA.getMessengerDna());
+//        myDna.printPeptide(protein);
+
+    }
+}
