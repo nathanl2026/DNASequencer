aa87e2dfd7fde0decc713751208056ce506bfa11
emlag <edl926@g.harvard.edu>
2020-04-13 09:24:55 +0800

    update instructions

---
+++ src/edu/cis/main/Main.java
@@ -0,0 +1,21 @@
+package edu.cis.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//     UNCOMMENT THE FOLLOWING CODE WHEN YOU'RE READY TO TEST
+
+//        DNAStrand myDna = new DNAStrand();
+//        Mrna myMRNA = new Mrna();
+//        Ribosome myRib = new Ribosome();
+//        myDna.readDNA("dnaSequence");
+//
+//        myMRNA.createCopy(myDna.getDnaSequence());
+//        ArrayList<String> protein = myRib.createProtein(myMRNA.getMessengerDna());
+//        for (String section : protein)
+//        {
+//            System.out.println(section);
+//        }
+    }
+}
