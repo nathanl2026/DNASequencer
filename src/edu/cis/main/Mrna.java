ebfd88019c6a0232edcf5ce300221580859d7ce6
nathanlau <nathanl2026@student.cis.edu.hk>
2024-05-27 16:11:56 +0800

    Finished Mrna

--- src/edu/cis/main/Mrna.java
+++ src/edu/cis/main/Mrna.java
@@ -3,25 +3,33 @@ package edu.cis.main;
 import java.util.ArrayList;
 
 public class Mrna {
-    ArrayList messengerDNA;
+    ArrayList<String> messengerDNA;
 
     public Mrna() {
-        messengerDNA = new ArrayList();
+        messengerDNA = new ArrayList<>();
     }
-    public void createCopy(ArrayList dnaList) {
+    public void createCopy(ArrayList<String> dnaList) {
         ArrayList<String> copyList = new ArrayList<>();
 
-
+        for (int i = 0; i < dnaList.size(); i++) {
+            String dnaNucleotide = dnaList.get(i);
+            StringBuilder nucleotideMrna = new StringBuilder();
+            for (int j = 0; j < dnaNucleotide.length(); j++) {
+                if (dnaNucleotide.charAt(j) == 'a') {
+                    nucleotideMrna.append('u');
+                }
+            }
+            copyList.add(nucleotideMrna.toString());
+        }
+        messengerDNA = copyList;
         //loop through each string in dnaList
         //for each string, we are going to create a copy of it
             //string ccc, we will create a copy that is now ccc
             //cac --> cuc
             // add the copy to copyList
-
-        messengerDNA = copyList;
     }
 
-    public ArrayList getMessengerDNA() {
+    public ArrayList<String> getMessengerDNA() {
         return messengerDNA;
     }
 }
